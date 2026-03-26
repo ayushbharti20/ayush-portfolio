54f6371ef964401870d6c646393810a8cfba0eb7
ayushbharti20 <ayushbharti20022005@gmail.com>
2026-03-26 18:06:40 +0530

    fix: restore github calendar import and add missing stats image css

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -3,7 +3,7 @@
 import dynamic from 'next/dynamic';
 
 const GitHubCalendar = dynamic(
-  () => import('react-github-calendar'),
+  () => import('react-github-calendar').then((mod) => mod.GitHubCalendar),
   { ssr: false, loading: () => <p>Loading activity...</p> }
 );
 
